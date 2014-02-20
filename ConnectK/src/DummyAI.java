8cdf051e20c9a76011b97a48572f4906b552eaca
putty174 <myh10232223@yahoo.com>
2014-02-19 21:59:39 -0800

    Single Chains

--- ConnectK/src/DummyAI.java
+++ ConnectK/src/DummyAI.java
@@ -1,13 +1,19 @@
 import connectK.CKPlayer;
 import connectK.BoardModel;
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 public class DummyAI extends CKPlayer {
 	private BoardModel board; //The BoardModel state so we don't have to pass this everywhere
 	private HashSet<Point> relaventMoves = new HashSet<Point>(); //A list of empty spots 8 way adjacent to already placed pieces 
-	private HashMap<Point, Chain>[] chains = new HashMap[2]; //An array of maps for chains. 0 index is me, 1 index is opponent
+	private Map<Point, Chain> myChains = new HashMap<Point, Chain>(); //An array of maps for our chains
+	private Map<Point, Chain> enemyChains = new HashMap<Point, Chain>(); //An array of maps for enemy chains
+	
+	private Point move; //The move we want to make
 	
 	private long start; //A timer to track when our turn started
 	
@@ -27,14 +33,13 @@ public class DummyAI extends CKPlayer {
 	@Override
 	public Point getMove(BoardModel state, int deadline) {
 		start = System.currentTimeMillis();
-		Point move;
+		readBoard();
 		board = state;
-		addRelaventMoves();
 		if(state.lastMove == null)
 			move = firstMove(state);
 		else
 			move = ids(state, 0);
-		return makeMove(move);
+		return makeMove();
 	}
 	
 	//If we're first, automatically make this move
@@ -42,11 +47,16 @@ public class DummyAI extends CKPlayer {
 		return new Point((state.getWidth() - 1) / 2,(state.getHeight() - 1) / 2);
 	}
 	
-	//Otherwise, think about what mvoe to make
+	//Otherwise, think about what move to make
 	private Point ids(BoardModel state, int depth) {
 		Point move = dumbMove(state);
 		
 		return move;
+	}
+	
+	private void readBoard() {
+		addEnemyRelaventMoves();
+		addEnemyChains();
 	}
 	
 	//Call whenever to check if we still have time
@@ -55,19 +65,49 @@ public class DummyAI extends CKPlayer {
 		return (end - start) < (deadline * 0.95 * 1000);
 	}
 	
-	//Build the list of moves that are 8 way adjacent to tiles already filled
-	private void addRelaventMoves() {
+	//Build the enemy list of moves that are 8 way adjacent to tiles already filled
+	private void addEnemyRelaventMoves() {
 		int x;
 		int y;
 		
 		for(int i = -1; i < 2; i++)
 			for(int j = -1; j < 2; j++)
-				if(i != 0 || j != 0) {
+				if(!(i == 0 && j == 0)) {
 					x = board.lastMove.x + i;
 					y = board.lastMove.y + j;
-					if (x > -1 && x < board.width && y > -1 && y < board.height && board.pieces[x][y] == 0)
+					if (isValid(x,y))
 						relaventMoves.add(new Point(x,y));
 				}
+	}
+	
+	//Removes any already used moves by both players
+		private void removeRelaventMoves() {
+			if(relaventMoves.contains(board.lastMove))
+				relaventMoves.remove(board.lastMove);
+			if(relaventMoves.contains(move))
+				relaventMoves.remove(move);
+		}
+	
+	private void addEnemyChains() {
+		int x1, y1, x2, y2;
+		Point pl = null;
+		Point pr = null;
+		List<Point> chain = new ArrayList<Point>();
+		for(int i = -1; i < 2; i++)
+			for(int j = -1; j < 2; j++)
+				if(!(i == 0 && j == 0)) {
+					x1 = board.lastMove.x + i;
+					x2 = board.lastMove.x - i;
+					y1 = board.lastMove.y + j;
+					y2 = board.lastMove.y - i;
+					if (isValid(x1,y1))
+						pl = new Point(x1,y1);
+					if (isValid(x2, y2))
+						pr = new Point(x2,y2);
+					chain.add(board.lastMove);
+					enemyChains.put(board.lastMove, new Chain(chain, pl, pr));
+				}
+		
 	}
 	
 	//Adds chains by both players to memory
@@ -76,19 +116,15 @@ public class DummyAI extends CKPlayer {
 	}
 	
 	//Cleans up memory before giving up turn
-	private Point makeMove(Point move){
+	private Point makeMove(){
 		addChains();
-		removeRelaventMoves(move);
+		removeRelaventMoves();
 		printRelaventMoves();
 		return move;
 	}
 	
-	//Removes any already used moves by both players
-	private void removeRelaventMoves(Point move) {
-		if(relaventMoves.contains(board.lastMove))
-			relaventMoves.remove(board.lastMove);
-		if(relaventMoves.contains(move))
-			relaventMoves.remove(move);
+	private boolean isValid(int x, int y) {
+		return ((x > -1 && x < board.width) && (y > -1 && y < board.height) && board.pieces[x][y] == 0);
 	}
 	
 	//Prints all relevant moves
@@ -100,10 +136,14 @@ public class DummyAI extends CKPlayer {
 	
 	//Helper class to track chains
 	private class Chain {
-		private Point start; //Bottom left most spot where the chain starts
-		private Point end; //Top right most sot where the chain ends
+		private List<Point> chain; //List of tiles that make up a chain
 		private Point left; //Spot needed to extend chain at Start
 		private Point right; //Spot needed to extend chain at End
+		public Chain(List<Point> c, Point l, Point r) {
+			chain = c;
+			left = l;
+			right = r;
+		}
 	}
 	
 	//Until we have an actual AI to test, just choose the far left bottom spot
